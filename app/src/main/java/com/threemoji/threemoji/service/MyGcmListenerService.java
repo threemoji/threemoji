cdea0d09137fc3c301a458ab9ada04dfab208dab
Sebastian Quek <sebq13@gmail.com>
2015-06-10 17:29:44 +0800

    Set up dependencies for GCM https://developers.google.com/cloud-messaging/android/client

---
+++ app/src/main/java/com/threemoji/threemoji/service/MyGcmListenerService.java
@@ -0,0 +1,6 @@
+package com.threemoji.threemoji.service;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+public class MyGcmListenerService extends GcmListenerService {
+}
---
+++ app/src/main/java/com/threemoji/threemoji/service/MyInstanceIDListenerService.java
@@ -0,0 +1,6 @@
+package com.threemoji.threemoji.service;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+}

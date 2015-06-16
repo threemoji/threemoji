1a7fc5c6e32f3e6f66165b28e96607a059a06bbc
Sebastian Quek <sebq13@gmail.com>
2015-06-16 22:59:46 +0800

    Added a EmojiList class which stores all the links to the emoji drawables

---
+++ app/src/androidTest/java/com/threemoji/threemoji/TestEmojiList.java
@@ -0,0 +1,14 @@
+package com.threemoji.threemoji;
+
+import android.test.AndroidTestCase;
+
+public class TestEmojiList extends AndroidTestCase {
+    public void testLengthOfAllEmoji() {
+        int totalNumberOfEmoji = EmojiList.emoticons.length +
+                                 EmojiList.nature.length +
+                                 EmojiList.objects.length +
+                                 EmojiList.places.length +
+                                 EmojiList.other.length;
+        assertEquals(EmojiList.allEmoji.length, totalNumberOfEmoji);
+    }
+}

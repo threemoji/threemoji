c30c61d533d1ebda9a9790c94dd13c57b929ef32
Sebastian Quek <sebq13@gmail.com>
2015-06-17 16:43:41 +0800

    Pressing back after changing emoji will not bring you to the old state of MainActivity

--- app/src/main/java/com/threemoji/threemoji/StartPageActivity.java
+++ app/src/main/java/com/threemoji/threemoji/StartPageActivity.java
@@ -124,6 +124,7 @@ public class StartPageActivity extends AppCompatActivity implements SelectEmojiD
                       .apply();
             setProfileGeneratedName();
             Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish();
         } else {

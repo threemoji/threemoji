3ed1ec2eb4322cf050a9cb67e31eda79bb4b25bc
Sebastian Quek <sebq13@gmail.com>
2015-06-29 21:54:20 +0800

    [#4] Updated the database helpers and implemented the content provider

--- app/src/main/java/com/threemoji/threemoji/data/ChatContract.java
+++ app/src/main/java/com/threemoji/threemoji/data/ChatContract.java
@@ -9,20 +9,21 @@ public class ChatContract {
     public static final String CONTENT_AUTHORITY = "com.threemoji.threemoji";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-    public static final String PATH_PARTNER = "partner";
-    public static final String PATH_THREAD = "thread";
+    public static final String PATH_PARTNERS = "partners";
+    public static final String PATH_MESSAGES = "messages";
 
     public static final class PartnerEntry implements BaseColumns {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
-                                                              .appendPath(PATH_PARTNER)
+                                                              .appendPath(PATH_PARTNERS)
                                                               .build();
         public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PARTNER;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" +
+                PATH_PARTNERS;
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" +
-                PATH_PARTNER;
+                PATH_PARTNERS;
 
-        public static final String TABLE_NAME = "partner";
+        public static final String TABLE_NAME = "partners";
 
         public static final String COLUMN_UUID = "uuid";
         public static final String COLUMN_EMOJI_1 = "emoji_1";
@@ -36,25 +37,48 @@ public class ChatContract {
         }
     }
 
-    public static final class ThreadEntry implements BaseColumns {
+    public static final class MessageEntry implements BaseColumns {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
-                                                              .appendPath(PATH_THREAD)
+                                                              .appendPath(PATH_MESSAGES)
                                                               .build();
         public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_THREAD;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" +
+                PATH_MESSAGES;
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" +
-                PATH_THREAD;
+                PATH_MESSAGES;
 
-        public static final String TABLE_NAME = "thread";
+        public static final String TABLE_NAME = "messages";
 
         public static final String COLUMN_PARTNER_KEY = "partner_id";
         public static final String COLUMN_DATETIME = "date_time";
         public static final String COLUMN_SENT_OR_RECEIVED = "sent_or_received";
-        public static final String COLUMN_MESSAGE = "message";
+        public static final String COLUMN_MESSAGE_DATA = "message_data";
 
-        public static Uri buildThreadUri(long id) {
+        public static Uri buildMessagesUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildMessagesWithPartner(String uuid, String emoji_1, String emoji_2,
+                                                   String emoji_3, String gender,
+                                                   String generated_name) {
+            return CONTENT_URI.buildUpon()
+                              .appendQueryParameter(PartnerEntry.COLUMN_UUID, uuid)
+                              .appendQueryParameter(PartnerEntry.COLUMN_EMOJI_1, emoji_1)
+                              .appendQueryParameter(PartnerEntry.COLUMN_EMOJI_2, emoji_2)
+                              .appendQueryParameter(PartnerEntry.COLUMN_EMOJI_3, emoji_3)
+                              .appendQueryParameter(PartnerEntry.COLUMN_GENDER, gender)
+                              .appendQueryParameter(PartnerEntry.COLUMN_GENERATED_NAME,
+                                                    generated_name).build();
+        }
+
+        public static String[] getQueryParamsFromUri(Uri uri) {
+            return new String[]{uri.getQueryParameter(PartnerEntry.COLUMN_UUID),
+                                uri.getQueryParameter(PartnerEntry.COLUMN_EMOJI_1),
+                                uri.getQueryParameter(PartnerEntry.COLUMN_EMOJI_2),
+                                uri.getQueryParameter(PartnerEntry.COLUMN_EMOJI_3),
+                                uri.getQueryParameter(PartnerEntry.COLUMN_GENDER),
+                                uri.getQueryParameter(PartnerEntry.COLUMN_GENERATED_NAME)};
         }
     }
 }
--- app/src/main/java/com/threemoji/threemoji/data/ChatDbHelper.java
+++ app/src/main/java/com/threemoji/threemoji/data/ChatDbHelper.java
@@ -1,7 +1,7 @@
 package com.threemoji.threemoji.data;
 
 import com.threemoji.threemoji.data.ChatContract.PartnerEntry;
-import com.threemoji.threemoji.data.ChatContract.ThreadEntry;
+import com.threemoji.threemoji.data.ChatContract.MessageEntry;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -27,23 +27,24 @@ public class ChatDbHelper extends SQLiteOpenHelper {
                 PartnerEntry.COLUMN_EMOJI_2 + " INTEGER NOT NULL, " +
                 PartnerEntry.COLUMN_EMOJI_3 + " INTEGER NOT NULL, " +
                 PartnerEntry.COLUMN_GENDER + " TEXT NOT NULL, " +
-                PartnerEntry.COLUMN_GENERATED_NAME + " TEXT NOT NULL " +
+                PartnerEntry.COLUMN_GENERATED_NAME + " TEXT NOT NULL, " +
+//                "PRIMARY KEY (" + PartnerEntry.COLUMN_UUID + ", " + PartnerEntry._ID + ") " +
                 " );";
 
-        final String SQL_CREATE_THREAD_TABLE =
-                "CREATE_TABLE " + ThreadEntry.TABLE_NAME + " (" +
-                ThreadEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                ThreadEntry.COLUMN_PARTNER_KEY + " INTEGER NOT NULL, " +
-                ThreadEntry.COLUMN_DATETIME + " BIGINT NOT NULL, " +
-                ThreadEntry.COLUMN_SENT_OR_RECEIVED + " TEXT NOT NULL, " +
-                ThreadEntry.COLUMN_MESSAGE + " TEXT NOT NULL, " +
+        final String SQL_CREATE_MESSAGE_TABLE =
+                "CREATE_TABLE " + MessageEntry.TABLE_NAME + " (" +
+                MessageEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MessageEntry.COLUMN_PARTNER_KEY + " INTEGER NOT NULL, " +
+                MessageEntry.COLUMN_DATETIME + " BIGINT NOT NULL, " +
+                MessageEntry.COLUMN_SENT_OR_RECEIVED + " TEXT NOT NULL, " +
+                MessageEntry.COLUMN_MESSAGE_DATA + " TEXT NOT NULL, " +
 
-                " FOREIGN KEY (" + ThreadEntry.COLUMN_PARTNER_KEY + ") REFERENCES " +
+                " FOREIGN KEY (" + MessageEntry.COLUMN_PARTNER_KEY + ") REFERENCES " +
                 PartnerEntry.TABLE_NAME + " (" + PartnerEntry._ID + ") " +
                 " );";
 
         db.execSQL(SQL_CREATE_PARTNER_TABLE);
-        db.execSQL(SQL_CREATE_THREAD_TABLE);
+        db.execSQL(SQL_CREATE_MESSAGE_TABLE);
     }
 
     @Override

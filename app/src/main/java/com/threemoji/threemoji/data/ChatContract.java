8916a91352f2bf79015030315636dd5852ec547a
Sebastian Quek <sebq13@gmail.com>
2015-07-05 16:45:59 +0800

    Added new people nearby table on client database

--- app/src/main/java/com/threemoji/threemoji/data/ChatContract.java
+++ app/src/main/java/com/threemoji/threemoji/data/ChatContract.java
@@ -12,6 +12,7 @@ public class ChatContract {
 
     public static final String PATH_PARTNERS = "partners";
     public static final String PATH_MESSAGES = "messages";
+    public static final String PATH_PEOPLE_NEARBY = "people_nearby";
 
     public static final class PartnerEntry implements BaseColumns {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
@@ -38,7 +39,6 @@ public class ChatContract {
         public static Uri buildPartnerUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
-
     }
 
     public static final class MessageEntry implements BaseColumns {
@@ -71,6 +71,33 @@ public class ChatContract {
 
         public static Uri buildMessagesWithPartnerUri(String uuid) {
             return CONTENT_URI.buildUpon().appendPath(uuid).build();
+        }
+    }
+
+    public static final class PeopleNearbyEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                                                              .appendPath(PATH_PEOPLE_NEARBY)
+                                                              .build();
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" +
+                PATH_PEOPLE_NEARBY;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" +
+                PATH_PEOPLE_NEARBY;
+
+        public static final String TABLE_NAME = "people";
+
+        public static final String COLUMN_UUID = "uuid";
+        public static final String COLUMN_EMOJI_1 = "emoji_1";
+        public static final String COLUMN_EMOJI_2 = "emoji_2";
+        public static final String COLUMN_EMOJI_3 = "emoji_3";
+        public static final String COLUMN_GENDER = "gender";
+        public static final String COLUMN_GENERATED_NAME = "generated_name";
+
+        public static Uri buildPeopleNearbyUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
         }
     }
 }

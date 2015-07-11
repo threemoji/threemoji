4bab3b25239526f1f5b6030e49b8e78c60c6c04e
John Yong <whipermr5@gmail.com>
2015-07-11 16:24:18 +0800

    Used default notifications preference string instead

--- app/src/main/java/com/threemoji/threemoji/service/MyGcmListenerService.java
+++ app/src/main/java/com/threemoji/threemoji/service/MyGcmListenerService.java
@@ -87,7 +87,7 @@ public class MyGcmListenerService extends GcmListenerService {
 
     private boolean isNotificationsEnabled() {
         return PreferenceManager.getDefaultSharedPreferences(this)
-                                .getBoolean(getString(R.string.pref_chat_notifications_key), true);
+                                .getBoolean(getString(R.string.pref_chat_notifications_key), Boolean.parseBoolean(getString(R.string.pref_chat_notifications_default)));
     }
 
     @Override
